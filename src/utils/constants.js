85e4f5635dae3480c88c5b8f393570a3959b7fa6
Taman-07 <tamanjot707@gmail.com>
2026-08-28 10:20:20 +0530

    constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,5 +5,12 @@
 // export const BASE_URL = "/api";
 
 
+// export const BASE_URL =
+//     location.hostname === "localhost" ? "http://localhost:3000" : "/api";
+
+
 export const BASE_URL =
-    location.hostname === "localhost" ? "http://localhost:3000" : "/api";
+    location.hostname === "localhost"
+        ? "http://localhost:3000"
+        : "https://devtinder-production-37f5.up.railway.app";
+        
